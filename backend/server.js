29b55ab126ae2c0b1d879ba9777bfbec3ba568c9
MI ALI AFRASH <155898544+aliafrash@users.noreply.github.com>
2026-09-06 09:43:04 +0530

    Implement JWT authentication and user roles

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,39 @@
-const dotenv = require("dotenv");
-dotenv.config();
-
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 
-const connectDB = require("./config/db");
+const connectDB=require("./config/db");
+
+const authRoutes=require("./routes/authRoutes");
+
+
+const app=express();
+
 
 connectDB();
 
-const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.json({
-    message: "TaskFlow API is running"
-  });
+
+// Routes
+app.use("/api/auth",authRoutes);
+
+
+
+app.get("/",(req,res)=>{
+
+    res.send("TaskFlow Backend API Running");
+
 });
 
-const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+const PORT=process.env.PORT || 5000;
+
+
+app.listen(PORT,()=>{
+
+    console.log(`Server running on port ${PORT}`);
+
 });
